3a2fe05f11d105e15645462e294d953885d61232
Christophe <chrismuzikos@gmail.com>
2016-06-06 15:36:42 +0200

    init repo

---
+++ dist/js/script.js
@@ -0,0 +1,18 @@
+var timer; //var for reload page
+
+$( document ).ready(function() {
+  window.mySwipe = new Swipe(document.getElementById('slider'), {
+    speed: 300,
+    auto: 3000, // define duration slide
+    continuous: true,
+    disableScroll: true,
+    stopPropagation: false,
+    callback: function(index, elem) {
+      // Reload page after 30s inactivity
+      window.clearTimeout(timer);
+      timer = window.setTimeout(function(){ document.location.href = 'index.html'; },30000);
+    },
+    transitionEnd: function(index, elem) {
+    }
+  });
+});
---
+++ src/readme.md
@@ -0,0 +1,5 @@
+Pour transformer un site web en app Videospot :
+  - rdv dans le folder
+  - selectionnez tous les fichiers
+  - compressez (ex : app.zip)
+  - uploadez sur manager.videospot.com
